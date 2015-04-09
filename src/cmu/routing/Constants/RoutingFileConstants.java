bf051a00020d74e1ae406c084ad93e492961466f
Garrick-Li <garrick.lee.lgq@gmail.com>
2015-04-09 01:20:18 -0500

    Create RoutingFileConstants.java

---
+++ src/cmu/routing/Constants/RoutingFileConstants.java
@@ -0,0 +1,41 @@
+package cmu.routing.constants;
+
+/**
+ * 
+ * This class holds all the constants that are present in the input file
+ * 
+ */
+public class RoutingFileConstants
+{
+    public static final String FEASIBLE_STRINGS = "num_strings";
+    public static final String DAILY_FLIGHT_LEGS = "num_flights";
+    public static final String GROUND_ARCS = "num_ground_arcs";
+    public static final String MAINTENANCE_LOC = "maint_loc";
+    public static final String COVER = "cover";
+    public static final String PROPOGATION_DELAY = "exp_prop_delay_strings";
+    public static final String COUNT_STRINGS = "count_strings";
+    public static final String COUNT_GROUND = "count_ground";
+    public static final String FLIGHT_MAINTENANCE_STRINGS_OUT = "flights_out_maint_strings";
+    public static final String FLIGHT_MAINTENANCE_STRINGS_IN = "flights_in_maint_strings";
+    public static final String FLIGHT_MAINTENANCE_GROUND_OUT = "flights_out_maint_ground";
+    public static final String FLIGHT_MAINTENANCE_GROUND_IN = "flights_in_maint_ground";
+    public static final String COVER_FAILURE_PROBABILITY = "cover_failure_probability";
+    public static final String COVER_HAT_VALUES = "cover_hat_values";
+    public static final String HAT_VALUE_STRINGS = "hat_values_strings";
+    public static final String STRINGS_SORTED_AS_HATS = "strings_sorted_as_hats";
+    public static final String ALPHA_PERCENTILE_PROPDELAY_STRINGS = "alpha_percentile_prop_delay_strings";
+    public static final String EXT_VAL_PROP_DELAY_STRINGS = "ext_val_prop_delay_strings";
+    public static final String AIRCRAFTS = "num_aircrafts";
+    public static final String COMMON_INPUT_FILE_PREFIX = "common_model_input_";
+    public static final String BASE_INPUT_FILE1 = "source/inputfile_base_1_B735.dat";
+    public static final String BASE_INPUT_FILE2 = "source/inputfile_base_2_B735.dat";
+    public static final String BASE_INPUT_FILE3 = "source/inputfile_base_3_B735.dat";
+    public static final String INCREMENTAL_INPUT_FILE_PREFIX = "inputfile_incremental_";
+    public static final String GAMMA = "gamma";
+    public static final String THRESHOLD = "threshold";
+    public static final String PROTECTION = "protection";
+    public static final String STRINGS_IN_SOLUTION = "strings_available_in_solution";
+    public static final String ALPHA_PERCENTILE_CONSTANT = "alpha_percentile_constant";
+    public static final String AVERAGE_PROP_DELAY_THRESHOLD = "average_prop_delay_threshold";
+    public static final String EXPECTED_PROP_DELAY_VALUE = "expected_prop_delay_value";
+}
